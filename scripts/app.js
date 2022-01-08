9875d799d7ac778bc50961c61e19c7fcb7d3a729
Tony Haunschmidt <tmhaunschmidt@gmail.com>
2022-01-08 09:33:03 +0000

    added spawn function v1

--- scripts/app.js
+++ scripts/app.js
@@ -12,7 +12,7 @@ function init() {
     for (let i = 0; i < gameGridCellCount; i++){
       const cell = document.createElement('div')
       cell.style.width = `${100 / gameGridWidth}%`
-      cell.style.height = `${100/ gameGridHeight}%`
+      cell.style.height = `${100 / gameGridHeight}%`
       cell.innerText = i //keep during coding and then delete
       gameGrid.appendChild(cell)
       gameGridCells.push(cell)
@@ -20,8 +20,7 @@ function init() {
   }
   generateGrid()
 
-
-//will need to play about with width and height to make it responsive
+  //will need to play about with width and height to make it responsive
   
 
   //DEFINING THE TETROMINOES
@@ -34,24 +33,31 @@ function init() {
   const ZTetromino = [[1, 1, 0], [0, 1, 1], [0, 0, 0]]
 
   const tetrominoes = [ITetromino, OTetromino, TTetromino, JTetromino, LTetromino, STetromino, ZTetromino]
-
+  
 
   //Testing a Spawn
-  function SpawnTetromino(firstSpawnPosition){
-    for (let i = 0; i < tetrominoes[1].length; i++){
-      for (let j = 0; j < tetrominoes[1].length; j++){
-        if (tetrominoes[0][i][j] === 1){
+  function SpawnTetromino(tetrominoesIndex, firstSpawnPosition){
+    for (let i = 0; i < tetrominoes[tetrominoesIndex].length; i++){
+      for (let j = 0; j < tetrominoes[tetrominoesIndex].length; j++){
+        if (tetrominoes[tetrominoesIndex][i][j] === 1){
           gameGridCells[firstSpawnPosition].classList.add('block')
+          console.log(gameGridCells[firstSpawnPosition])
           console.log(firstSpawnPosition)
         }
         firstSpawnPosition ++
       }
-      firstSpawnPosition += gameGridWidth
+      firstSpawnPosition += (gameGridWidth - tetrominoes[tetrominoesIndex].length)
     }
   }
-  SpawnTetromino(1)
-  //not quite working- not sure why. this is console logging the wrong cells but also not changing adding the class to any?
-  //also, will need to add tetromino argument. 
+  SpawnTetromino(0, 259)
+  SpawnTetromino(1, 9)
+  SpawnTetromino(2, 55)
+  SpawnTetromino(3, 115)
+  SpawnTetromino(4, 198)
+  SpawnTetromino(5, 171)
+  SpawnTetromino(6, 231)   
+  
+  //need to add a check for spawning into wall
 
 
 }
